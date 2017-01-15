3674141b1513f92c79cabdc08c85b2321ec79dad
Unknown <acmp2013@mail.ru>
2017-01-16 05:46:47 +1100

    Nichego vajnogo ne sdelal

---
+++ azazaz.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class azazaz here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class azazaz extends Actor
+{
+    /**
+     * Act - do whatever the azazaz wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     * azaza
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
