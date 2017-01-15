9fce1113a37cbc27606b716a8d9ac280f26fadee
John Strange <hitman7020@mail.ru>
2017-01-16 06:23:13 +1000

    Перемещение кнопки

--- Gold.java
+++ Gold.java
@@ -13,14 +13,28 @@ public class Gold extends Chackers
      * Act - do whatever the Gold wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private boolean _clicked = false;
+    
     public void act() 
     {   
-        if(Greenfoot.mouseClicked(this))
+        if (_clicked)
+        {
+            if ( Greenfoot.getMouseInfo().getButton() == 1 )
+            {
+                int _x = Greenfoot.getMouseInfo().getX();
+                int _y = Greenfoot.getMouseInfo().getY();
+                setLocation(_x, _y);
+                setImage("white.png");
+                _clicked = false;
+            }
+        }
+        
+        if(Greenfoot.mouseClicked(this) && !_clicked)
         {
             int x = getX();
             int y = getY();
             setImage("white_2.png");
-            
+            _clicked = true;
         }
-    }    
+   }    
 }
