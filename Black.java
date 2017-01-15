2d9a4c0e0e13a7029f7da5df8eb9eee6ad9a5afe
John Strange <hitman7020@mail.ru>
2017-01-16 06:31:36 +1000

    Добавил для черных

--- Black.java
+++ Black.java
@@ -12,14 +12,35 @@ public class Black extends Chackers
      * Act - do whatever the Black wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+   private boolean _clicked = false;
+   private boolean _clicked2 = false;
+    
     public void act() 
     {
-        if(Greenfoot.mouseClicked(this))
+        if (_clicked)
+        {
+            if ( Greenfoot.getMouseInfo().getButton() == 1 )
+            {
+                int _x = Greenfoot.getMouseInfo().getX();
+                int _y = Greenfoot.getMouseInfo().getY();
+                setLocation(_x, _y);
+                setImage("black.png");
+                _clicked = false;
+                _clicked2 = true;
+            }
+        }
+        
+        if(Greenfoot.mouseClicked(this) && !_clicked && !_clicked2)
         {
             int x = getX();
             int y = getY();
             setImage("black_2.png");
-            
+            _clicked = true;
+        }
+        
+        if(Greenfoot.mouseClicked(this))
+        {
+            _clicked2 = false;
         }
     }    
 }
